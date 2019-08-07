80aa456533d0c1571fd6e8e9ef7180729a2ea12c
Ryden Butler <ryden.winfield@gmail.com>
2019-08-07 12:00:58 -0500

    Upload Lab 2
    - Add class02 lab

---
+++ day02/lab02.py
@@ -0,0 +1,23 @@
+## Fill in the following methods for the class 'Clock'
+
+class Clock:
+    def __init__(self, hour, minutes):
+        self.minutes = minutes
+        self.hour = hour
+
+    ## Print the time
+    def __str__(self):
+    
+    ## Add time
+    ## Don't return anything
+    def __add__(self,minutes):
+    
+    ## Subtract time
+    ## Don't return anything
+    def __sub__(self,minutes):
+    
+    ## Are two times equal?
+    def __eq__(self, other):
+    
+    ## Are two times not equal?
+    def __ne__(self, other):
